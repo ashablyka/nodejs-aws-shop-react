116fa5a70a303e1a6658117608df3fc756be0b91
Andrei Shablyka <andrei_shablyka@epam.com>
2023-05-31 17:49:22 +0400

    add comment

--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,6 +1,7 @@
 // jest-dom adds custom jest matchers for asserting on DOM nodes.
 // allows you to do things like:
-// expect(element).toHaveTextContent(/react/i)
+// expect(element).toHaveTextContent(/react/i)++
+// test comment
 // learn more: https://github.com/testing-library/jest-dom
 import "@testing-library/jest-dom";
 import { server } from "~/mocks/server";
